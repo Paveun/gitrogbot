ee5abab64c5009537d1c20944db40f26c09dfc50
Paveun <a.tresierrajansen@gmail.com>
2023-09-22 12:35:17 -0400

    remove deleted file

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,4 @@
 from src.bot import run_discord_bot
 from src.frogfacts import get_frogfact
-from src.text2dict import text_to_dict
 from src.url import print_url
 from src.weather import get_weather
