cfce0fb8891b76427cb37f24621962d37720b962
Paveun <a.tresierrajansen@gmail.com>
2023-09-22 12:32:24 -0400

    add text2dict function into frogfacts.py

--- src/frogfacts.py
+++ src/frogfacts.py
@@ -1,13 +1,24 @@
 import random
 
-import src
-
+def text_to_dict(file):
+  with open(file, 'r', encoding='utf-8') as file:
+    dict = {}
+    for line in file:
+      parts = line.strip().split('. ', 1)
+      if len(parts) == 2:
+        key = int(parts[0])
+        value = str(parts[1])
+        dict[key] = value
+  return dict
 
 def get_frogfact(file):
   # with open('resources/responses.txt', 'r', encoding='utf-8') as file:
   #   responses = file.readlines()
   # random_response = random.choice(responses).strip()
-  facts_dict = src.text_to_dict(file)
-  random_entry = random.sample(facts_dict.items(), 1)
-  random_response = random_entry[0][1]
+  facts_dict = text_to_dict(file)
+  random_entry = random.choice(list(facts_dict.items()))
+  random_response = random_entry[1]
   return random_response
+
+# print(text_to_dict('resources/responses.txt'))
+# print(get_frogfact('resources/responses.txt'))
